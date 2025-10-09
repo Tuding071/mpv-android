6e92dfe309c418d802bc527dda2846c247088301
Tuding071 <davidpenaranda071@gmail.com>
2025-10-10 02:54:08 +0800

    Update MPVActivity.kt

--- app/src/main/java/is/xyz/mpv/MPVActivity.kt
+++ app/src/main/java/is/xyz/mpv/MPVActivity.kt
@@ -2078,4 +2078,3 @@ class MPVActivity : AppCompatActivity(), MPVLib.EventObserver, TouchGesturesObse
         // precision used by seekbar (1/s)
         private const val SEEK_BAR_PRECISION = 2
     }
-}
